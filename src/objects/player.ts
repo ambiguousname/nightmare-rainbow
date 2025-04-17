d3ba290284c8e4c878adaac070b06175a61b64ce
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-04-17 11:14:09 -0700

    Add air friction

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -3,7 +3,7 @@ import { Vector2 } from "../util";
 const PLAYER_SETTINGS = {
 	rotateSpeed: 0.5,
 	acceleration: 2,
-	airFriction: 0.01
+	airFriction: 0.04
 }
 
 export class Player extends Phaser.Physics.Matter.Sprite {
@@ -31,17 +31,22 @@ export class Player extends Phaser.Physics.Matter.Sprite {
 		this.addToDisplayList();
 	}
 
+	get up() {
+		let forwardAngle = (this.body as MatterJS.BodyType).angle;
+		return {x: Math.cos(forwardAngle), y: Math.sin(forwardAngle) };
+	}
+
 	move(intent : Vector2, delta : number) {
 		let delta_ms = delta/1000;
 
 		this.scene.matter.body.rotate(this.body as MatterJS.BodyType, intent.x * delta_ms * PLAYER_SETTINGS.rotateSpeed);
-
-		let forwardAngle = (this.body as MatterJS.BodyType).angle;
 		
 		let vel = this.getVelocity();
 
-		let up = {x: Math.cos(forwardAngle) * intent.y, y: Math.sin(forwardAngle) * intent.y };
+		let up = this.up;
 
-		this.setVelocity(vel.x + up.x * delta_ms * PLAYER_SETTINGS.acceleration, vel.y + up.y * delta_ms * PLAYER_SETTINGS.acceleration);
+		// TODO: I think a tire based system would be better.
+
+		this.setVelocity(vel.x + up.x * intent.y * delta_ms * PLAYER_SETTINGS.acceleration, vel.y + up.y * intent.y * delta_ms * PLAYER_SETTINGS.acceleration);
 	}
 }

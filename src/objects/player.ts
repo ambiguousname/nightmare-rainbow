4008db17b5482eb3cda6d6819cd8e831349e7381
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-04-18 10:58:29 -0700

    Start on wheel controls

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -11,6 +11,9 @@ const PLAYER_HEIGHT = 25;
 
 export class Player extends Phaser.Physics.Matter.Sprite {
 
+	#leftWheel : Phaser.GameObjects.Components.Transform;
+	#rightWheel : Phaser.GameObjects.Components.Transform;
+
 	constructor(world : Phaser.Physics.Matter.World, x : number, y: number, texture: string) {
 		super(world, x, y, texture, "", {
 			// Stickiness on walls:
@@ -27,39 +30,49 @@ export class Player extends Phaser.Physics.Matter.Sprite {
 			}
 		});
 
-		// Do not rotate unless we want:
-		this.scene.matter.body.setInertia(this.body as MatterJS.BodyType, Infinity);
+		this.scene.matter.body.setInertia(this.body as MatterJS.BodyType, 5000);
+
+		this.#leftWheel = this.scene.add.rectangle(PLAYER_WIDTH/2, PLAYER_HEIGHT/2, 2.5, 10, 0xff0000);
+		this.#rightWheel = this.scene.add.rectangle(-PLAYER_WIDTH/2, PLAYER_HEIGHT/2, 2.5, 10, 0xffffff);
 
 		this.addToUpdateList();
 		this.addToDisplayList();
 	}
 
-	get up() {
-		let forwardAngle = (this.body as MatterJS.BodyType).angle;
-		return {x: Math.cos(forwardAngle), y: Math.sin(forwardAngle) };
+	updateWheels(turn : number, delta_ms : number) : number {
+		let angle = (this.body as MatterJS.BodyType).angle;
+
+		let leftWheelAngle = angle + 7 * Math.PI/8;
+		let rightWheelAngle = angle + 9 * Math.PI/8;
+
+		let wheelTurn = angle + Math.PI/2 + turn;
+		
+		this.#leftWheel.setPosition(Math.cos(leftWheelAngle) * PLAYER_WIDTH/2 + this.body.position.x, Math.sin(leftWheelAngle) * PLAYER_WIDTH/2 + this.body.position.y, -1);
+		this.#leftWheel.setRotation(wheelTurn);
+
+		this.#rightWheel.setPosition(Math.cos(rightWheelAngle) * PLAYER_WIDTH/2 + this.body.position.x, Math.sin(rightWheelAngle) * PLAYER_WIDTH/2 + this.body.position.y);
+		this.#rightWheel.setRotation(wheelTurn);
+
+		return wheelTurn - Math.PI/2;
 	}
 
 	move(intent : Vector2, delta : number) {
 		let delta_ms = delta/1000;
 
-		let angle = (this.body as MatterJS.BodyType).angle;
+		// this.scene.matter.body.rotate(this.body as MatterJS.BodyType, intent.x * delta_ms * PLAYER_SETTINGS.rotateSpeed);
 
-		let wheelLeft = {x: Math.cos(angle) * PLAYER_WIDTH/2 + this.body.position.x, y: Math.sin(angle) * PLAYER_HEIGHT/2 + this.body.position.y};
+		let angle = this.updateWheels(intent.x, delta_ms);
 
-		let wheelRight = {x: -PLAYER_WIDTH/2 * Math.cos(angle) + this.body.position.x, y: Math.sin(angle) * PLAYER_HEIGHT/2};
-
-		this.scene.matter.body.rotate(this.body as MatterJS.BodyType, intent.x * delta_ms * PLAYER_SETTINGS.rotateSpeed);
-
-		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, wheelLeft, delta_ms * PLAYER_SETTINGS.acceleration * intent.y, angle /* TODO: Get this working with wheel angle */);
+		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, {x: this.#leftWheel.x, y: this.#leftWheel.y}, delta_ms * PLAYER_SETTINGS.acceleration * intent.y, angle);
 		
-		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, wheelRight, delta_ms * PLAYER_SETTINGS.acceleration * intent.y, angle /* TODO: Get this working with wheel angle */);
+		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, {x: this.#rightWheel.x, y: this.#rightWheel.y }, delta_ms * PLAYER_SETTINGS.acceleration * intent.y, angle);
 
 		// TODO: Need to either re-direct or slow down velocity that is not aligned with wheels.
 		// Steps here:
-		// 1. Create wheel sprite objects (for visual clarity)
-		// 2. Left and right arrow keys set rotation of the wheels relative to the car
-		// 3. When you accelerate, you accelerate in the direction the wheels are pointed in.
+		// 1. Create wheel sprite objects (for visual clarity) (done)
+		// 2. Left and right arrow keys set rotation of the wheels relative to the car (done)
+		// 3. When you accelerate, you accelerate in the direction the wheels are pointed in. (done)
 		// 4. Multiply the current velocity by sin(angle) and cos(angle), to get only the velocity pointed in the same direction as the angle.
-		// 5. Disable setting rotation, torque should *hopefully* handle that for us.
+		// 5. Disable setting rotation, torque should *hopefully* handle that for us. (done)
 	}
 }

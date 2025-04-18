1ab363b990ffa0966ce3ddb010d3129627a4d246
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-04-17 20:48:44 -0700

    Start on wheel system

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -2,9 +2,12 @@ import { Vector2 } from "../util";
 
 const PLAYER_SETTINGS = {
 	rotateSpeed: 2,
-	acceleration: 0.1,
+	acceleration: 0.05,
 	airFriction: 0.04
 }
+
+const PLAYER_WIDTH = 50;
+const PLAYER_HEIGHT = 25;
 
 export class Player extends Phaser.Physics.Matter.Sprite {
 
@@ -19,8 +22,8 @@ export class Player extends Phaser.Physics.Matter.Sprite {
 			chamfer: 0.5,
 			
 			shape: {
-				width: 50,
-				height: 25
+				width: PLAYER_WIDTH,
+				height: PLAYER_HEIGHT
 			}
 		});
 
@@ -39,10 +42,18 @@ export class Player extends Phaser.Physics.Matter.Sprite {
 	move(intent : Vector2, delta : number) {
 		let delta_ms = delta/1000;
 
+		let angle = (this.body as MatterJS.BodyType).angle;
+
+		let wheelLeft = {x: Math.cos(angle) * PLAYER_WIDTH/2 + this.body.position.x, y: Math.sin(angle) * PLAYER_HEIGHT/2 + this.body.position.y};
+
+		let wheelRight = {x: -PLAYER_WIDTH/2 * Math.cos(angle) + this.body.position.x, y: Math.sin(angle) * PLAYER_HEIGHT/2};
+
 		this.scene.matter.body.rotate(this.body as MatterJS.BodyType, intent.x * delta_ms * PLAYER_SETTINGS.rotateSpeed);
 
-		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, this.body.position, delta_ms * PLAYER_SETTINGS.acceleration * intent.y);
+		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, wheelLeft, delta_ms * PLAYER_SETTINGS.acceleration * intent.y, angle /* TODO: Get this working with wheel angle */);
+		
+		this.scene.matter.applyForceFromPosition(this.body as MatterJS.BodyType, wheelRight, delta_ms * PLAYER_SETTINGS.acceleration * intent.y, angle /* TODO: Get this working with wheel angle */);
 
-		// TODO: I think a tire based system would be better.
+		// TODO: Need to either re-direct or slow down velocity that is not aligned with wheels.
 	}
 }

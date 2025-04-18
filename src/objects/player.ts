da611221b64a8f67e2a14ddbc59fb3ac87fdbc34
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-04-17 20:54:19 -0700

    One last step

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -60,5 +60,6 @@ export class Player extends Phaser.Physics.Matter.Sprite {
 		// 2. Left and right arrow keys set rotation of the wheels relative to the car
 		// 3. When you accelerate, you accelerate in the direction the wheels are pointed in.
 		// 4. Multiply the current velocity by sin(angle) and cos(angle), to get only the velocity pointed in the same direction as the angle.
+		// 5. Disable setting rotation, torque should *hopefully* handle that for us.
 	}
 }
